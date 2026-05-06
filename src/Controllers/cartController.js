04112b5864d5931d2d307418c34b59908ab45e86
Mohd Ashiq <ashiqm9292@gmail.com>
2026-05-06 14:28:51 +0530

    create cart functionality

--- src/Controllers/cartController.js
+++ src/Controllers/cartController.js
@@ -1,59 +1,80 @@
+import mongoose from "mongoose";
 import Cart from "../Models/cartModal.js";
 
 // add to cart
 export const addToCart = async (req, res) => {
     try {
-        const { productId } = req.body;
-        if (!productId) {
-            return res.status(400).json({ message: 'Please provide product id' });
+        const { productId, name, size, price } = req.body;
+        if (!productId || !name || !size || !price) {
+            return res.status(400).json({ success: false, message: 'Please provide product id, name, size and price' });
         }
-        const newCart = new Cart({
+        const existingCart = await Cart.findOne({ productId, user: req.user._id });
+        if (existingCart) {
+            const updatedCart = await Cart.find({ user: req.user._id }).populate('productId');
+            return res.status(200).json({ success: true, message: 'Product already in cart', cart: updatedCart });
+        }
+        const cart = new Cart({
             user: req.user._id,
-            productId
-        });
-        await newCart.save();
-        res.status(201).json({ message: 'add to cart successfully', cart: newCart });
+            productId,
+            name,
+            size,
+            price
+        })
+        await cart.save();
+        const updatedCart = await Cart.find({ user: req.user._id }).populate('productId');
+        return res.status(201).json({ success: true, message: 'add to cart successfully', cart: updatedCart });
     } catch (error) {
-        res.status(500).json({ message: 'error in adding to cart', error: error.message });
+        return res.status(500).json({ success: false, message: 'error in adding to cart', error: error.message });
     }
 };
 
 // get cart 
 export const getCart = async (req, res) => {
     try {
-        const cart = await Cart.find({ user: req.user._id }).populate('productId');
-        if (!cart || cart.length === 0) {
-            return res.status(404).json({ message: 'NO Cart Items' });
+        const cart = await Cart.find({ user: req.user._id }).populate('productId', 'name price size quantity');
+        if (!cart) {
+            return res.status(200).json({ success: true, message: 'Cart not found' });
         }
-        res.status(200).json({ cart });
+        return res.status(200).json({ success: true, message: 'Cart found successfully', cartlenght: cart.length, cart });
     } catch (error) {
-        res.status(500).json({ message: 'error in getting cart', error: error.message });
+        return res.status(500).json({ message: 'error in getting cart', error: error.message });
     }
 };
 
 // delete cart 
 export const deleteCart = async (req, res) => {
     try {
-        const cart = await Cart.findOneAndDelete({ _id: req.params.id, user: req.user._id });
-        if (!cart) {
-            return res.status(404).json({ message: 'Cart not found' });
+        const item = await Cart.findOneAndDelete({ _id: req.params.id, user: req.user._id });
+        if (!item) {
+            return res.status(200).json({ success: true, message: 'Item already removed' });
         }
-        res.status(200).json({ message: 'Cart deleted successfully' });
+        const upadatedCart = await Cart.find({ user: req.user._id }).populate('productId');
+        return res.status(200).json({ success: true, message: 'Cart deleted successfully', cart: updatedCart });
     } catch (error) {
-        res.status(500).json({ message: 'error in deleting cart', error: error.message });
+        return res.status(500).json({
+            success: false,
+            message: 'error in deleting cart',
+            error: error.message
+        });
     }
 };
+
 
 // update quantity
 export const updateQuantity = async (req, res) => {
     try {
         const { quantity } = req.body;
+        if (!quantity) {
+            return res.status(400).json({ message: 'Please provide quantity' });
+        }
         const cart = await Cart.findOneAndUpdate({ _id: req.params.id, user: req.user._id }, { $set: { quantity } }, { returnDocument: 'after' });
         if (!cart) {
             return res.status(404).json({ message: 'Cart not found' });
         }
-        res.status(200).json({ message: 'Quantity updated successfully', cart });
+        // updated cart quantity
+        const updatedCart = await Cart.find({ user: req.user._id }).populate('productId');
+        return res.status(200).json({ message: 'Quantity updated successfully', cart: updatedCart });
     } catch (error) {
-        res.status(500).json({ message: 'error in updating quantity', error: error.message });
+        return res.status(500).json({ message: 'error in updating quantity', error: error.message });
     }
 };
--- src/Models/cartModal.js
+++ src/Models/cartModal.js
@@ -11,6 +11,18 @@ const cartSchema = new mongoose.Schema({
         ref: 'Product',
         required: true
     },
+    name: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
     quantity: {
         type: Number,
         default: 1
